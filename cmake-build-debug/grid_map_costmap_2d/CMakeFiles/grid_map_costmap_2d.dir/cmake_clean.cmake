4c33ec802ee6ca9f97950c9b57309fd82df0f1ac
Beat Scherrer <b.scherrer@mt-robot.com>
2019-03-13 16:11:06 +0100

    remove build folder, added build folders to gitignore

--- cmake-build-debug/grid_map_costmap_2d/CMakeFiles/grid_map_costmap_2d.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libgrid_map_costmap_2d.pdb"
-  "libgrid_map_costmap_2d.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/grid_map_costmap_2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/grid_map_costmap_2d/CMakeFiles/grid_map_costmap_2d.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libgrid_map_costmap_2d.a"
-)

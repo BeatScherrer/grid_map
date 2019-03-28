bb3d792d5a8282225d83ac6a015301f5d57eef79
Beat Scherrer <b.scherrer@mt-robot.com>
2019-03-28 11:41:17 +0100

    added support for oldschool cmake and mtr cmake macros.

--- grid_map_core/grid_map_coreConfig.cmake
+++ grid_map_core/grid_map_coreConfig.cmake
@@ -1,2 +1,18 @@
+# - Config file for the grid_map_core package
+# It defines the following variables
+#  grid_map_core_INCLUDE_DIRS - include directories for grid_map_core
+#  grid_map_core_LIBRARIES    - libraries to link against
+#  grid_map_core_EXECUTABLE   - the grid_map_core executable
+
 include("${CMAKE_CURRENT_LIST_DIR}/grid_map_coreTargets.cmake")
 
+# ------------------------------------------------------------------------------
+# Old CMake Support
+# ------------------------------------------------------------------------------
+
+# Support Uppercase name
+set(GRID_MAP_CORE_FOUND TRUE)
+
+# Set find_package variables
+get_target_property(grid_map_core_INCLUDE_DIRS grid_map_core::grid_map_core INTERFACE_INCLUDE_DIRECTORIES)
+set(GRID_MAP_CORE_INTERFACE_INCLUDE_DIRECTORIES)

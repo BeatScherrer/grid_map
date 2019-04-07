15bd3e9ed1b55d887812f1f0f4680b080349ba6d
BeatScherrer <beat.scherrer@gmail.com>
2019-04-07 11:32:53 +0200

    Updated CMakeLists to use PCL correctly

--- grid_map_sdf/grid_map_sdfConfig.cmake
+++ grid_map_sdf/grid_map_sdfConfig.cmake
@@ -3,16 +3,20 @@
 #  grid_map_core_INCLUDE_DIRS - include directories for grid_map_core
 #  grid_map_core_LIBRARIES    - libraries to link against
 #  grid_map_core_EXECUTABLE   - the grid_map_core executable
-include("${CMAKE_CURRENT_LIST_DIR}/grid_map_coreTargets.cmake")
+include(CMakeFindDependencyMacro)
+find_dependency(grid_map_core CONFIG REQUIRED)
+find_dependency(PCL CONFIG REQUIRED COMPONENTS)
+
+include("${CMAKE_CURRENT_LIST_DIR}/grid_map_sdfTargets.cmake")
 
 # ------------------------------------------------------------------------------
 # Old CMake & Glorious MTR_Macro support
 # ------------------------------------------------------------------------------
 
 # Support Uppercase name
-set(GRID_MAP_CORE_FOUND TRUE)
+set(GRID_MAP_SDF_FOUND TRUE)
 
 # Set find_package variables
-get_target_property(grid_map_core_INCLUDE_DIRS grid_map_core::grid_map_core INTERFACE_INCLUDE_DIRECTORIES)
-set(grid_map_core_LIBRARY grid_map_core)
-set(grid_map_core_LIBRARY_DIR "/usr/lib/")
+get_target_property(grid_map_sdf_INCLUDE_DIRS grid_map_sdf::grid_map_sdf INTERFACE_INCLUDE_DIRECTORIES)
+set(grid_map_sdf_LIBRARY grid_map_sdf)
+set(grid_map_sdf_LIBRARY_DIR "/usr/lib/")
